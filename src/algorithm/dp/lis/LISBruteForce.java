69b22d420971341449412d1c102ad97f2a215136
dijadhav <dipakjadhav08@gmail>
2022-01-26 19:33:00 +0530

    Updated the loop

--- src/algorithm/dp/lis/LISBruteForce.java
+++ src/algorithm/dp/lis/LISBruteForce.java
@@ -32,7 +32,7 @@ public class LISBruteForce {
 		int len = 0;
 		for (int i = 0; i < arr.length; i++) {
 			int l = 1;
-			for (int j = 0;  j< arr.length-1; j++) {
+			for (int j = i;  j< arr.length-1; j++) {
 				if (arr[j] < arr[j+1]) {
 					l++;
 				} 
